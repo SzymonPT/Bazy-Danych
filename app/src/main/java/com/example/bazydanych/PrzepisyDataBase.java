bde33a434eac90b471abb195ce0f72a0fb3bb6fa
SzymonPT <polytopia72@gmail.com>
2026-02-13 10:57:43 +0100

    dao

---
+++ app/src/main/java/com/example/bazydanych/PrzepisyDataBase.java
@@ -0,0 +1,6 @@
+package com.example.bazydanych;
+
+import androidx.room.RoomDatabase;
+
+public abstract class PrzepisyDataBase extends RoomDatabase {
+}
---
+++ app/src/main/java/com/example/bazydanych/WypiekiDao.java
@@ -0,0 +1,26 @@
+package com.example.bazydanych;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+public interface WypiekiDao {
+    @Insert
+    public void wstawWypiekiDoBazy(Wypieki wypieki);
+    @Insert
+    public void wstawKilkaWypiekow(Wypieki wypieki);
+    @Delete
+    public void usunZBazy(Wypieki wypieki);
+    @Update
+    public void zaktualizuj(Wypieki wypieki);
+    @Query("Select *  From `wypieki_tabela`")
+    List<Wypieki> zwrocWszystkieWypiekiZBazy();
+
+    @Query("Select `nazwa` From `wypieki_tabela` Where czas_pieczenia < :czas")
+    List<String> zwrocNazwyWypiekow(int czas);
+
+}
